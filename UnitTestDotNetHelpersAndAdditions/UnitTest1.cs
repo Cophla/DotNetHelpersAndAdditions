40e03a76511aa699003b725757aa1b9ad8470646
Mohamed Sobeh <msobeh@scryptech.com>
2019-02-17 07:40:45 +0200

    added unit test project

---
+++ UnitTestDotNetHelpersAndAdditions/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDotNetHelpersAndAdditions
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
